ffb4f1786d2d9878eba6f3a1d819f68ea36a4144
burmer <83092581+burmer@users.noreply.github.com>
2021-05-09 16:22:50 +0300

    ViewBag Kullanımı

---
+++ .Net Core Asp Mvc/ViewBag Kullanımı.cs
@@ -0,0 +1,19 @@
+/*
+Controller Bölümü 
+
+public IActionResult Index6()
+{
+	ViewBag.deger1 = "Core dersleri devam ediyor";
+	ViewBag.deger2 = "Merhaba bu bir deneme viewbagı";
+	return View();
+}
+
+View Bölümü
+
+ <div>
+	@ViewBag.deger1
+	<br />
+	@ViewBag.deger2
+</div>
+
+*/
