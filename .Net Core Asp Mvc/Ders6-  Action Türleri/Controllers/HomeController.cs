0d9d2d1156f502936e1d929cfc739f78c95db62b
burmer <83092581+burmer@users.noreply.github.com>
2021-10-09 20:55:14 +0300

    -

---
+++ .Net Core Asp Mvc/Ders6-  Action Türleri/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public int Index3()
+        {
+            return 25;
+        }
+        public string Index4()
+        {
+            return "Merhaba bugün hava iyi";
+        }
+
+    }
+}
