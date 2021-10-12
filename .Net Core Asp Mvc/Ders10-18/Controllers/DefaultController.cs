2fe11157c128534ec3a4bba2f88941bd5f8c8ee1
burmer <83092581+burmer@users.noreply.github.com>
2021-10-12 21:44:08 +0300

    -

---
+++ .Net Core Asp Mvc/Ders10-18/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KitaplikProjesi.Models;
+
+namespace KitaplikProjesi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ktp = new List<Kitaplar>()
+            {
+                new Kitaplar(){ID=1,KitapAd="Satranç",Yazar="Zweig"},
+                new Kitaplar(){ID=2,KitapAd="Dr Oxun Deneyi",Yazar="Jules Verne"},
+                new Kitaplar(){ID=3,KitapAd="Deli Fişek",Yazar="Vacenceulos"},
+                new Kitaplar(){ID=4,KitapAd="Kırmızı Bisiklet",Yazar="Muzaffer İzgü"},
+                new Kitaplar(){ID=5,KitapAd="Toros Canavarı",Yazar="Aziz Nesin"}
+            };
+            return View(ktp);
+        }
+
+        public IActionResult deneme()
+        {
+            return View();
+        }
+        public IActionResult StatikTema()
+        {
+            return View();
+        }
+    }
+}
---
+++ .Net Core Asp Mvc/Ders10-18/ViewComponents/SonKitaplar.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KitaplikProjesi.Models;
+
+
+namespace KitaplikProjesi.ViewComponents
+{
+    public class SonKitaplar: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var kitapllistesi = new List<Kitaplar>
+            {
+                new Kitaplar
+                {
+                    ID=6,
+                    KitapAd="Korku",
+                    Yazar= "Zweig"
+                },
+                new Kitaplar
+                {
+                    ID=7,
+                    KitapAd="Anna Karanina",
+                    Yazar="Tolstoy"
+                }
+            };
+            return View(kitapllistesi);
+        }
+    }
+}
