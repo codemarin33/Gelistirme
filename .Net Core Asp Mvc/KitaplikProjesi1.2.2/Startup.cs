4425430d2fe1e4b2e26417d25fa0663a74cdd068
burmer <83092581+burmer@users.noreply.github.com>
2021-10-24 16:06:03 +0300

    -

---
+++ .Net Core Asp Mvc/KitaplikProjesi1.2.2/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitaplikProjesi_v1._1._2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //mvc yapýlandýrmasý ekledik
+            services.AddControllersWithViews();
+            services.AddAuthentication
+            (CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(x =>
+             { 
+               x.LoginPath = "/Login/GirisYap/";            
+             });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Birim}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
